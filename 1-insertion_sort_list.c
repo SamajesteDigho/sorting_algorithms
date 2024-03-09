1301cf340cf41be917b3b09fe10ad8c01865f057
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-09 20:30:37 +0800

    commiting

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,7 @@
 void insertion_sort_list(listint_t **list)
 {
 listint_t *curr, *front, *back;
-if (list == NULL || *list == NULL)
+if (list == NULL || (*list) == NULL)
 return;
 curr = (*list);
 while (curr->next != NULL)
