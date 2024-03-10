6c2d629b5e9520135c3b645fa63d57b2cdec78ff
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-11 07:46:21 +0800

    Commiting

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -4,8 +4,44 @@
  * @array: Array to sort
  * @size: Size of the array
  * Description: Sort array using the Counting sort algorithm.
-*/
+ */
 void counting_sort(int *array, size_t size)
 {
+    int i, j, max, n;
+    int *count, *output;
+    if (array == NULL || size == 0)
+        return;
+    n = (int)size;
+    max = 0;
+    for (i = 0; i < (int)size; i++)
+        if (array[i] > max)
+            max = array[i];
+    max += 1;
+    count = (int *)malloc(max);
+    output = (int *)malloc(size);
+    for (i = 0; i < max; i++)
+        count[i] = 0;
 
+    for (i = 0; i < (int)size; i++)
+        output[i] = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        j = i; /*array[i];*/
+        count[j] = count[j] + 1;
+    }
+
+    for (i = 0; i < max; i++)
+        count[i] = count[i] + count[i - 1];
+
+    for (i = n; i >= 0; i--)
+    {
+        j = i; /*array[i];*/
+        count[j] = count[j] - 1;
+        output[count[j]] = array[i];
+    }
+    print_array(count, max);
+
+    for (i = 0; i < n; i++)
+        array[i] = output[i];
 }
--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,5 +1,78 @@
 #include "sort.h"
 /**
+ * print_array_partly - Merge Sort
+ * @arr: Array to sort
+ * @start: Start
+ * @end: End
+ * @side: Side
+ * Description: Sort array using the Merge sort algorithm.
+*/
+void print_array_partly(int *arr, int start, int end, char *side)
+{
+int i;
+printf("%s", side);
+for (i = start; i < end; i++)
+{
+printf("%d", arr[i]);
+if (i < end - 1)
+printf(", ");
+}
+printf("\n");
+}
+
+/**
+ * TopDownMerge - Merge Sort
+ * @B: Array to sort
+ * @begin: Start
+ * @middle: Middle
+ * @end: End
+ * @A: Array to sort
+ * Description: Sort array using the Merge sort algorithm.
+*/
+void TopDownMerge(int *B, int begin, int middle, int end, int *A)
+{
+int i, j, k;
+i = begin;
+j = middle;
+for (k = begin; k < end; k++)
+{
+if (i < middle && (j >= end || A[i] <= A[j]))
+{
+B[k] = A[i];
+i += 1;
+}
+else
+{
+B[k] = A[j];
+j += 1;
+}
+}
+printf("Merging...\n");
+print_array_partly(A, begin, middle, "[left]: ");
+print_array_partly(A, middle, end, "[right]: ");
+print_array_partly(B, begin, end, "[Done]: ");
+}
+
+/**
+ * TopDownSplitMerge - Merge Sort
+ * @B: Array to sort
+ * @begin: Start
+ * @end: End
+ * @A: Array to sort
+ * Description: Sort array using the Merge sort algorithm.
+*/
+void TopDownSplitMerge(int *B, int begin, int end, int *A)
+{
+int middle;
+if (end - begin <= 1)
+return;
+middle = (end + begin) / 2;
+TopDownSplitMerge(A, begin, middle, B);
+TopDownSplitMerge(A, middle, end, B);
+TopDownMerge(B, begin, middle, end, A);
+}
+
+/**
  * merge_sort - Merge Sort
  * @array: Array to sort
  * @size: Size of the array
@@ -7,5 +80,14 @@
 */
 void merge_sort(int *array, size_t size)
 {
-
+int n, i;
+int *duplicate;
+if (array == NULL || size == 0)
+return;
+n = (int) size;
+duplicate = (int *) malloc(size);
+for (i = 0; i < n; i++)
+duplicate[i] = array[i];
+TopDownSplitMerge(array, 0, n, duplicate);
+free(duplicate);
 }
